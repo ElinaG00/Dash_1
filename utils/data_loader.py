77b8c1aab4df56e9d15000004aedd3f7ba8fc61b
ElinaG00 <gurina0elina@gmail.com>
2025-10-24 16:40:46 +0300

    Units

---
+++ utils/data_loader.py
@@ -0,0 +1,35 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+API_URL = os.getenv("API_URL")
+
+def load_data(city):
+    response = requests.get(API_URL, params={'key': API_KEY, 'q': city, 'days': 1})
+    data = response.json()
+    city_name = data['location']['name']
+    temp = data['current']['temp_c']
+    condition = data['current']['condition']['text']
+    icon = data['current']['condition']['icon']
+
+    forecast_hours = data['forecast']['forecastday'][0]['hour']
+    hours = [hour['time'][-5:] for hour in forecast_hours]
+    temps = [hour['temp_c'] for hour in forecast_hours]
+    ap = [hour['pressure_mb'] * 0.75 for hour in forecast_hours]
+    wind = [hour['wind_kph'] / 3.6 for hour in forecast_hours]
+    wind_dirs = [hour['wind_degree'] for hour in forecast_hours]
+
+    return {
+        'city_name': city_name,
+        'temp': temp,
+        'condition': condition,
+        'icon': icon,
+        'temps': temps,
+        'ap': ap,
+        'wind': wind,
+        'wind_dirs': wind_dirs,
+        'hours': hours
+    }
